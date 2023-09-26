07d4f86c51c24bb7ff23fc65fbc0cb7ede8281a9
Avazkhon <avazkhon1996@gmail.com>
2023-09-26 11:39:34 +0500

    Hi

--- components/HeaderOnly.tsx
+++ components/HeaderOnly.tsx
@@ -9,7 +9,7 @@ const inter = Inter({
   weight: ["100", "400", "600", "700"],
 });
 
-export default function Header() {
+export default function HeaderOnly() {
   return (
     <section className={inter.className + " "}>
       <header className="w-100 bg bg-[url('/images/truck_bg.jpg')] bg-cover bg-right-bottom pb-10 text-neutral-800 sm:bg-bottom lg:h-screen">
